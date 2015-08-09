2e495aeea6848fa3ace5a328fb8a5ca03888521c
BlankedVerse <colinm64@hotmail.com>
2015-08-09 17:48:24 -0400

    ResourcePickup class created
    Created the scripts used for resource pickups - the bits of mineral
    created when a mine is... mined... Currently just contains a getter for
    the mineral's value (publically set by the Unity editor) and an
    OnTriggerEnter2D method for collision with a WarpPad.

---
+++ Assets/Scripts/ResourcePickup.cs
@@ -0,0 +1,48 @@
+﻿/*
+* Filename:		ResourcePickup.cs
+* Programmer:	Colin McMillan
+* Date:			August 2015
+* Description:	Describes the actions and behaviours of the warp pad.
+*/
+
+
+using UnityEngine;
+using System.Collections;
+
+
+// Name:	ResourcePickup
+// Purpose:	The behaviours and 
+public class ResourcePickup : LiftableObject
+{
+	// The value of the resource
+	public int value;
+
+
+	// Name:		Value()
+	// Description:	Returns the value of this pickup.
+	// Returns:		The value of the pickup.
+	public int Value()
+	{
+		return value;
+	}
+
+
+	/*
+	// Name:		OnTriggerEnter2D()
+	// Description:	Processes reactions to entering trigger regions on the map.
+	// Parameters:	Collider2D trigger	- The trigger region entered.
+	protected void OnTriggerEnter2D (Collider2D trigger)
+	{
+		//base.OnTriggerEnter2D(triggerZone);
+		
+		// Check for an exit
+		if (trigger.tag == "WarpPad")
+		{
+			//ResourcePickup resourceToWarp = trigger.GetComponent<ResourcePickup>();
+			
+			Player.AcquireMineral(value);
+			
+			Destroy(this.gameObject);
+		}
+	}*/
+}
