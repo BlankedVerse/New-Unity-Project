8843832c0b7abd554c8f43f40652032ad969cea8
BlankedVerse <colinm64@hotmail.com>
2015-08-09 17:50:04 -0400

    WarpPad introduced
    The script for WarpPads. Currently possesses only an OnTriggerEnter2D()
    method, for when a resource is dropped on the WarpPad. (The
    corresponding method on ResourcePickups is commented out, presently.)

---
+++ Assets/Scripts/WarpPad.cs
@@ -0,0 +1,38 @@
+﻿/*
+* Filename:		WarpPad.cs
+* Programmer:	Colin McMillan
+* Date:			August 2015
+* Description:	Describes the actions and behaviours of the warp pad.
+*/
+
+
+using UnityEngine;
+using System.Collections;
+
+
+// Name:	WarpPad
+// Purpose:	The behaviours of a warp pad, which can retrieve the player's dropped-off resources.
+public class WarpPad : MonoBehaviour
+{
+
+	// Name:		OnTriggerEnter2D()
+	// Description:	Processes reactions to entering trigger regions on the map.
+	//				Including: Doors/exits
+	// Parameters:	Collider2D trigger	- The trigger region entered.
+	protected void OnTriggerEnter2D (Collider2D trigger)
+	{
+		//base.OnTriggerEnter2D(triggerZone);
+		
+		// Check for an exit
+		if (trigger.tag == "Resource")
+		{
+			ResourcePickup resourceToWarp = trigger.GetComponent<ResourcePickup>();
+
+			Player.AcquireMineral(resourceToWarp.Value());
+
+			Destroy(trigger.gameObject);
+
+			//this.GetComponent<Animator>().
+		}
+	}
+}
